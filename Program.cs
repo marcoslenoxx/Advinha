a6ff990a8ec59ddb43c6e95f0ad704d3c5dc79ee
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-17 19:46:08 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("O que é o que é...");
+Thread.Sleep(1000);
+Console.WriteLine("Um pontinho preto no avião?");
+Thread.Sleep(5000);
+Console.WriteLine("Uma aeromosca");
